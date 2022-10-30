dff6374f2dd8d37b6e1a65419734bf54b746297a
wassim chettaoui <wassim.chettaoui@esprit.tn>
2022-10-30 16:00:27 +0100

    new

--- src/test/java/tn/esprit/rh/achat/StockServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/StockServiceTest.java
@@ -1,6 +1,7 @@
 package tn.esprit.rh.achat;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
